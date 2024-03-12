5f9062a46015146c324ce574d3d928a349c48f0c
ertugrulcan-ays <ertugrul.bakas@ayasofyazilim.com>
2024-03-12 11:36:21 +0300

    Initialization

---
+++ .storybook/main.ts
@@ -0,0 +1,56 @@
+import type { StorybookConfig } from '@storybook/react-webpack5';
+import path from 'path';
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-webpack5-compiler-swc',
+    '@storybook/addon-styling-webpack',
+    {
+      name: '@storybook/addon-styling-webpack',
+
+      options: {
+        rules: [
+          {
+            test: /\.css$/,
+            sideEffects: true,
+            use: [
+              require.resolve('style-loader'),
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  importLoaders: 1,
+                },
+              },
+              {
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  implementation: require.resolve('postcss'),
+                },
+              },
+            ],
+          },
+        ],
+      },
+    },
+  ],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  async webpackFinal(config) {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, '../@'),
+      };
+    }
+    return config;
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+export default config;
---
+++ vitest.config.mts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: { alias: { '@': '/@' } },
+  test: {
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js',
+    passWithNoTests: true,
+    coverage: {
+      include: ['{src,tests}/**/*'],
+    },
+  },
+});
