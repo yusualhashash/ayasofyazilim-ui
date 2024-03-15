413b2be332a60a428219e19709dce721b8d6d24c
Eren <eren.odaci@ayasofyazilim.com>
2024-03-15 11:08:01 +0300

    style(component): login page prefix removed

--- src/Login.tsx
+++ src/Login.tsx
@@ -6,12 +6,12 @@ import { cn } from '@/lib/utils';
 import { ShadcnButton } from './ShadcnButton';
 
 const themeVariants = cva(
-  'ay-flex md:ay-flex-1 ay-justify-center ay-items-center ay-px-5 ay-py-5',
+  'flex md:flex-1 justify-center items-center px-5 py-5',
   {
     variants: {
       variant: {
-        ayasofyazilim: 'ay-bg-zinc-900 ay-text-white',
-        abc1: 'ay-bg-emerald-900 ay-text-white',
+        ayasofyazilim: 'bg-zinc-900 text-white',
+        abc1: 'bg-emerald-900 text-white',
       },
     },
     defaultVariants: {
@@ -25,27 +25,25 @@ export type LoginProps = {
 };
 
 export const Login = ({ variant, ...props }: LoginProps) => (
-  <div className="ay-flex ay-flex-col md:ay-flex-row ay-h-screen">
+  <div className="flex flex-col md:flex-row h-screen">
     <div className={cn(themeVariants({ variant }))}>
-      <div className=" ay-flex ay-flex-row ay-items-center">
-        {props?.company}
-      </div>
+      <div className=" flex flex-row items-center">{props?.company}</div>
     </div>
-    <div className="ay-flex ay-flex-1 ay-container">
-      <div className="ay-mx-auto ay-flex ay-w-full ay-flex-col ay-justify-center ay-space-y-6 sm:ay-w-[350px]">
-        <div className="ay-flex ay-flex-col ay-space-y-2 ay-text-center">
-          <h1 className="ay-text-2xl font-semibold tracking-tight">
+    <div className="flex flex-1 container">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="flex flex-col space-y-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
             Create an account
           </h1>
           <p className="text-sm text-muted-foreground">
             Enter your email below to create your account
           </p>
         </div>
-        <div className="ay-grid ay-gap-6">
+        <div className="grid gap-6">
           <form>
-            <div className="ay-grid ay-gap-4">
-              <div className="ay-grid ay-gap-1">
-                <Label className="ay-sr-only" htmlFor="email">
+            <div className="grid gap-4">
+              <div className="grid gap-1">
+                <Label className="sr-only" htmlFor="email">
                   Email
                 </Label>
                 <Input
@@ -60,12 +58,12 @@ export const Login = ({ variant, ...props }: LoginProps) => (
               <ShadcnButton rounded="full">Sign In with Email</ShadcnButton>
             </div>
           </form>
-          <div className="ay-relative">
-            <div className="ay-absolute ay-inset-0 ay-flex ay-items-center">
-              <span className="ay-w-full ay-border-t" />
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <span className="w-full border-t" />
             </div>
-            <div className="ay-relative ay-flex ay-justify-center ay-text-xs ay-uppercase">
-              <span className="ay-bg-background ay-px-2 ay-text-muted-foreground">
+            <div className="relative flex justify-center text-xs uppercase">
+              <span className="bg-background px-2 text-muted-foreground">
                 Or continue with
               </span>
             </div>
@@ -74,13 +72,13 @@ export const Login = ({ variant, ...props }: LoginProps) => (
             <img
               src="https://cdn.e-devlet.gov.tr/themes/izmir/images/favicons/favicon-196x196.1.8.0.png"
               width={20}
-              className="ay-mx-1"
+              className="mx-1"
               alt="E-devlet"
             />
-            <span className="ay-mx-1">E-devlet </span>
+            <span className="mx-1">E-devlet </span>
           </ShadcnButton>
         </div>{' '}
-        <p className="px-8 ay-text-center text-sm text-muted-foreground">
+        <p className="px-8 text-center text-sm text-muted-foreground">
           By clicking continue, you agree to our{' '}
           <a
             href="/terms"
