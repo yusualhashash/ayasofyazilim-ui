b17d670977fe814136cb85d43066526946af22ab
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-03-20 15:25:42 +0300

    feat(organism): add card list

---
+++ src/organisms/cardList/CardList.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+
+import CardList from './CardList';
+
+export default {
+  title: 'Organism/Card list',
+  component: CardList,
+  argTypes: {},
+  parameters: {
+    layout: 'centered',
+  },
+} as Meta<typeof CardList>;
+
+const Template: StoryFn<typeof CardList> = (args) => <CardList {...args} />;
+
+export const CardListStory = Template.bind({});
+
+CardListStory.args = {
+  cards: [
+    {
+      title: 'Paid',
+      content: '15%',
+      description: 'Number of paid taxes',
+      footer: 'Your target is 100%',
+    },
+    {
+      title: 'People',
+      content: '15k',
+      description: 'Number of people in the system',
+      footer: 'Your target is 20K',
+    },
+    {
+      title: 'WIP',
+      content: '1',
+      description: 'Number of WIP refunds',
+      footer: 'Your target is 0',
+    },
+  ],
+};
---
+++ src/organisms/cardList/CardList.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import InfoCard, { infoCardProps } from '../../molecules/infocard/infocard';
+
+export type CardListPorps = {
+  cards: Array<infoCardProps>;
+};
+
+export default function CardList(porps: CardListPorps) {
+  return (
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      {porps.cards.map((card: infoCardProps) => (
+        <InfoCard key={card.title.replace(' ', '_')} {...card} />
+      ))}
+    </div>
+  );
+}
