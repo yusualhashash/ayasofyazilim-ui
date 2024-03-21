2cb0b118371289a0897861d799a0996706590d2f
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-03-21 08:15:23 +0300

    style(organism): cardlist extend large screen to 4

--- src/organisms/cardList/CardList.tsx
+++ src/organisms/cardList/CardList.tsx
@@ -8,7 +8,7 @@ export type CardListPorps = {
 
 export default function CardList(porps: CardListPorps) {
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
       {porps.cards.map((card: infoCardProps) => (
         <InfoCard key={card.title.replace(' ', '_')} {...card} />
       ))}
