88623ceac1adef2540fb98109fa5e664080d8347
Eren <eren.odaci@ayasofyazilim.com>
2024-03-21 12:17:47 +0300

    feat(novel component): add novel & custom novel editor template

---
+++ src/organisms/novel/extensions.tsx
@@ -0,0 +1,267 @@
+'use client';
+
+import { cx } from 'class-variance-authority';
+import {
+  CheckSquare,
+  Code,
+  Heading1,
+  Heading2,
+  Heading3,
+  ImageIcon,
+  List,
+  ListOrdered,
+  Text,
+  TextQuote,
+} from 'lucide-react';
+import {
+  Command,
+  HorizontalRule,
+  Placeholder,
+  StarterKit,
+  TaskItem,
+  TaskList,
+  TiptapImage,
+  TiptapLink,
+  UpdatedImage,
+  createSuggestionItems,
+  renderItems,
+} from 'novel/extensions';
+import { UploadImagesPlugin } from 'novel/plugins';
+import React from 'react';
+import { uploadFn } from './image-upload';
+
+export const suggestionItems = createSuggestionItems([
+  {
+    title: 'Text',
+    description: 'Just start typing with plain text.',
+    searchTerms: ['p', 'paragraph'],
+    icon: <Text size={18} />,
+    command: ({ editor, range }) => {
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .toggleNode('paragraph', 'paragraph')
+        .run();
+    },
+  },
+  {
+    title: 'To-do List',
+    description: 'Track tasks with a to-do list.',
+    searchTerms: ['todo', 'task', 'list', 'check', 'checkbox'],
+    icon: <CheckSquare size={18} />,
+    command: ({ editor, range }) => {
+      editor.chain().focus().deleteRange(range).toggleTaskList().run();
+    },
+  },
+  {
+    title: 'Heading 1',
+    description: 'Big section heading.',
+    searchTerms: ['title', 'big', 'large'],
+    icon: <Heading1 size={18} />,
+    command: ({ editor, range }) => {
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .setNode('heading', { level: 1 })
+        .run();
+    },
+  },
+  {
+    title: 'Heading 2',
+    description: 'Medium section heading.',
+    searchTerms: ['subtitle', 'medium'],
+    icon: <Heading2 size={18} />,
+    command: ({ editor, range }) => {
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .setNode('heading', { level: 2 })
+        .run();
+    },
+  },
+  {
+    title: 'Heading 3',
+    description: 'Small section heading.',
+    searchTerms: ['subtitle', 'small'],
+    icon: <Heading3 size={18} />,
+    command: ({ editor, range }) => {
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .setNode('heading', { level: 3 })
+        .run();
+    },
+  },
+  {
+    title: 'Bullet List',
+    description: 'Create a simple bullet list.',
+    searchTerms: ['unordered', 'point'],
+    icon: <List size={18} />,
+    command: ({ editor, range }) => {
+      editor.chain().focus().deleteRange(range).toggleBulletList().run();
+    },
+  },
+  {
+    title: 'Numbered List',
+    description: 'Create a list with numbering.',
+    searchTerms: ['ordered'],
+    icon: <ListOrdered size={18} />,
+    command: ({ editor, range }) => {
+      editor.chain().focus().deleteRange(range).toggleOrderedList().run();
+    },
+  },
+  {
+    title: 'Quote',
+    description: 'Capture a quote.',
+    searchTerms: ['blockquote'],
+    icon: <TextQuote size={18} />,
+    command: ({ editor, range }) =>
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .toggleNode('paragraph', 'paragraph')
+        .toggleBlockquote()
+        .run(),
+  },
+  {
+    title: 'Code',
+    description: 'Capture a code snippet.',
+    searchTerms: ['codeblock'],
+    icon: <Code size={18} />,
+    command: ({ editor, range }) =>
+      editor.chain().focus().deleteRange(range).toggleCodeBlock().run(),
+  },
+  {
+    title: 'Image',
+    description: 'Upload an image from your computer.',
+    searchTerms: ['photo', 'picture', 'media'],
+    icon: <ImageIcon size={18} />,
+    command: ({ editor, range }) => {
+      editor.chain().focus().deleteRange(range).run();
+      // upload image
+      const input = document.createElement('input');
+      input.type = 'file';
+      input.accept = 'image/*';
+      input.onchange = async () => {
+        if (input.files?.length) {
+          const file = input.files[0];
+          const pos = editor.view.state.selection.from;
+          uploadFn(file, editor.view, pos);
+        }
+      };
+      input.click();
+    },
+  },
+]);
+
+const slashCommand = Command.configure({
+  suggestion: {
+    items: () => suggestionItems,
+    render: renderItems,
+  },
+});
+const placeholder = Placeholder;
+
+const tiptapImage = TiptapImage.extend({
+  addProseMirrorPlugins: () => [
+    UploadImagesPlugin({
+      imageClass: cx('opacity-40 rounded-lg border border-stone-200'),
+    }),
+  ],
+}).configure({
+  allowBase64: true,
+  HTMLAttributes: {
+    class: cx('rounded-lg border border-muted'),
+  },
+});
+
+const updatedImage = UpdatedImage.configure({
+  HTMLAttributes: {
+    class: cx('rounded-lg border border-muted'),
+  },
+});
+
+const tiptapLink = TiptapLink.configure({
+  HTMLAttributes: {
+    class: cx(
+      'text-muted-foreground underline underline-offset-[3px] hover:text-primary transition-colors cursor-pointer'
+    ),
+  },
+});
+
+const taskList = TaskList.configure({
+  HTMLAttributes: {
+    class: cx('not-prose pl-2'),
+  },
+});
+const taskItem = TaskItem.configure({
+  HTMLAttributes: {
+    class: cx('flex items-start my-4'),
+  },
+  nested: true,
+});
+
+const horizontalRule = HorizontalRule.configure({
+  HTMLAttributes: {
+    class: cx('mt-4 mb-6 border-t border-muted-foreground'),
+  },
+});
+
+const starterKit = StarterKit.configure({
+  bulletList: {
+    HTMLAttributes: {
+      class: cx('list-disc list-outside leading-3 -mt-2'),
+    },
+  },
+  orderedList: {
+    HTMLAttributes: {
+      class: cx('list-decimal list-outside leading-3 -mt-2'),
+    },
+  },
+  listItem: {
+    HTMLAttributes: {
+      class: cx('leading-normal -mb-2'),
+    },
+  },
+  blockquote: {
+    HTMLAttributes: {
+      class: cx('border-l-4 border-primary'),
+    },
+  },
+  codeBlock: {
+    HTMLAttributes: {
+      class: cx(
+        'rounded-sm bg-muted border p-5 font-mono font-medium text-slate-700'
+      ),
+    },
+  },
+  code: {
+    HTMLAttributes: {
+      class: cx('rounded-md bg-muted  px-1.5 py-1 font-mono font-medium'),
+      spellcheck: 'false',
+    },
+  },
+  horizontalRule: false,
+  dropcursor: {
+    color: '#DBEAFE',
+    width: 4,
+  },
+  gapcursor: false,
+});
+
+export const extensions = [
+  starterKit,
+  placeholder,
+  tiptapLink,
+  tiptapImage,
+  updatedImage,
+  taskList,
+  taskItem,
+  horizontalRule,
+  slashCommand,
+];
