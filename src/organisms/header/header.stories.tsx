6b3e6db47b2ab8b40b969f7ef7ebb6736937361d
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-03-21 08:14:38 +0300

    feat(organism): header

---
+++ src/organisms/header/header.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+
+import DashboardHeader from './header';
+
+export default {
+  title: 'Organism/Header',
+  component: DashboardHeader,
+  argTypes: {},
+} as Meta<typeof DashboardHeader>;
+
+const Template: StoryFn<typeof DashboardHeader> = (args) => (
+  <DashboardHeader {...args} />
+);
+
+export const HeaderStory = Template.bind({});
+
+HeaderStory.args = {
+  heading: 'header',
+  text: 'test',
+};
