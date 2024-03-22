1ffda5bc620fc514716399dd1b991e35bf8b9bac
ertugrulcan-ays <ertugrul.bakas@ayasofyazilim.com>
2024-03-22 11:32:05 +0300

    feat(country-selector): country-selector props updated

--- src/organisms/country-selector/country-selector.tsx
+++ src/organisms/country-selector/country-selector.tsx
@@ -16,106 +16,112 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from '../../../@/components/ui/popover';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '../../../@/components/ui/tooltip';
 
 type CountrySelectItem = {
   label?: string;
   rtl?: boolean;
-  showLabel?: boolean;
   value?: string;
 };
-const countries = [
-  {
-    value: 'tr',
-    label: 'Türkçe',
-  },
-  {
-    value: 'it',
-    label: 'Italiano',
-  },
-  {
-    value: 'jp',
-    label: '日本語',
-  },
-  {
-    value: 'sa',
-    label: 'عربي',
-    rtl: true,
-  },
-];
 
 type CountrySelectorProps = {
+  countries?: Array<CountrySelectItem>;
   defaultValue?: CountrySelectItem;
   direction: 'ltr' | 'rtl';
   menuAlign: 'start' | 'center' | 'end';
   onValueChange?: () => {};
   searchEmptyValue?: string;
   searchText?: string;
+  showLabel?: boolean;
+  tooltipText?: string;
 };
+
 function CountrySelector({
   searchText,
   searchEmptyValue,
   defaultValue,
   menuAlign,
   direction,
+  tooltipText,
+  showLabel = false,
+  countries = [],
   onValueChange,
 }: CountrySelectorProps) {
   const [open, setOpen] = React.useState(false);
   const [value, setValue] = React.useState<string | undefined>('');
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          role="combobox"
-          variant="ghost"
-          aria-expanded={open}
-          className={`justify-between border-none bg-transparent px-2 gap-2 ${direction === 'rtl' ? 'flex-row-reverse' : ''}`}
-        >
-          {value ? (
-            <SelectedCountry
-              {...countries.find((country) => country.value === value)}
-              showLabel={false}
-            />
-          ) : (
-            <SelectedCountry {...defaultValue} showLabel={false} />
-          )}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[160px] p-0" align={menuAlign}>
-        <Command>
-          <CommandList>
-            <CommandInput placeholder={searchText} className="h-9 text-xs" />
-            <CommandEmpty>{searchEmptyValue}</CommandEmpty>
-            <CommandGroup>
-              {countries.map((country) => (
-                <CommandItem
-                  key={country.value}
-                  value={country.value}
-                  onSelect={(currentValue: string) => {
-                    setValue(currentValue);
-                    setOpen(false);
-                    if (onValueChange) onValueChange();
-                  }}
-                >
-                  <SelectedCountry {...country} />
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
+    <TooltipProvider>
+      <Tooltip>
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger>
+            <TooltipTrigger>
+              <Button
+                role="combobox"
+                variant="ghost"
+                aria-expanded={open}
+                className={`justify-between border-none bg-transparent px-2 gap-2 ${direction === 'rtl' ?? 'flex-row-reverse'}`}
+              >
+                {value ? (
+                  <SelectedCountry
+                    {...countries.find((country) => country.value === value)}
+                    showLabel={showLabel}
+                  />
+                ) : (
+                  <SelectedCountry {...defaultValue} showLabel={showLabel} />
+                )}
+              </Button>
+            </TooltipTrigger>
+            {tooltipText ? <TooltipContent>{tooltipText}</TooltipContent> : ''}
+          </PopoverTrigger>
+          <PopoverContent className="w-[160px] p-0" align={menuAlign}>
+            <Command>
+              <CommandList>
+                <CommandInput
+                  placeholder={searchText}
+                  className="h-9 text-xs"
+                />
+                <CommandEmpty>{searchEmptyValue}</CommandEmpty>
+                <CommandGroup>
+                  {countries.map((country) => (
+                    <CommandItem
+                      key={country.value}
+                      value={country.value}
+                      onSelect={(currentValue: string) => {
+                        setValue(currentValue);
+                        setOpen(false);
+                        if (onValueChange) onValueChange();
+                      }}
+                    >
+                      <SelectedCountry {...country} />
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              </CommandList>
+            </Command>
+          </PopoverContent>
+        </Popover>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
 
+type SelectedCountryProps = CountrySelectItem & {
+  showLabel?: boolean;
+};
 const SelectedCountry = ({
   value = '',
   label,
   rtl,
   showLabel = true,
-}: CountrySelectItem) => (
+}: SelectedCountryProps) => (
   <div
-    className={`${rtl ? 'flex-row-reverse' : ''}  flex w-full justify-between gap-2 overflow-hidden`}
+    className={`${rtl ?? 'flex-row-reverse'}  flex w-full justify-between gap-2 overflow-hidden items-center`}
   >
     {showLabel && <span className="text-xs">{label}</span>}
     <div className="border  rounded-sm overflow-hidden">
