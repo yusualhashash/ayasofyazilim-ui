8b976ac5706e46877628216747713ca07c24ca16
Abdurrahman Rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-03-29 10:00:16 +0300

    feat: add children to main layout (#33)

--- src/templates/mainlayout/index.tsx
+++ src/templates/mainlayout/index.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
-
+import React, { JSXElementConstructor, ReactElement } from 'react';
 import DashboardHeader from '../../organisms/header';
 import Sidebar from '../../molecules/side-bar/index';
 
 export type mainLayoutProps = {
-  logo: string;
-  title: string;
+  children: ReactElement<any, string | JSXElementConstructor<any>>;
+  logo?: string;
+  title?: string;
 };
 
-export default function Mainlayout() {
+export default function Mainlayout({ logo, title, children }: mainLayoutProps) {
   // porps: DashboardProps
   return (
     <>
       <DashboardHeader />
-      <Sidebar />
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-col w-full">
+          {`${logo} ${title}`}
+          {children}
+        </div>
+      </div>
     </>
   );
 }
