97d8d6ad52620cc6f19f0833da4eb872ee05f8a1
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-04-02 11:08:00 +0300

    chore: add navigation  menu links

--- src/organisms/header/header.stories.tsx
+++ src/organisms/header/header.stories.tsx
@@ -5,6 +5,7 @@ import DashboardHeader from '.';
 import Navigation from '../../molecules/navigation-menu';
 import AvatarWrapper from '../../molecules/avatar';
 import { UserNav } from '../profile-menu/index';
+import { navigationLinks } from '../../molecules/navigation-menu/data';
 
 export default {
   component: DashboardHeader,
@@ -25,4 +26,5 @@ export const HeaderStory = Template.bind({});
 HeaderStory.args = {
   title: 'ayasofya',
   logo: '',
+  navMenu: navigationLinks,
 };
