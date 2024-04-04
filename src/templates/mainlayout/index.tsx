6f6b620f5a47f4e711aae5debfab3d017186b5a4
Ertuğrulcan Bakas <ecbakas@gmail.com>
2024-04-04 16:41:49 +0300

    style:mainlayout

--- src/templates/mainlayout/index.tsx
+++ src/templates/mainlayout/index.tsx
@@ -2,7 +2,8 @@ import React, { JSXElementConstructor, ReactElement } from 'react';
 import { userNavTypes } from 'src/organisms/profile-menu';
 import DashboardHeader from '../../organisms/header';
 import Sidebar, { MenuProps } from '../../molecules/side-bar/index';
-import { navigationLinkTypes } from '../../molecules/navigation-menu';
+import { navigationLinkTypes } from '../../molecules/navigation-menu/types';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 export type mainLayoutProps = {
   children?: ReactElement<any, string | JSXElementConstructor<any>> | String;
@@ -25,7 +26,7 @@ export default function Mainlayout({
 }: mainLayoutProps) {
   // porps: DashboardProps
   return (
-    <div className="h-dvh grid grid-rows-[max-content_1fr]">
+    <div className="h-dvh grid grid-rows-[max-content_1fr]  overflow-hidden">
       <DashboardHeader
         logo={logo}
         title={title}
@@ -33,9 +34,11 @@ export default function Mainlayout({
         navMenu={navMenu}
         extraMenu={extraMenu}
       />
-      <div className="flex">
-        <Sidebar className="hidden md:flex" menus={menus} />
-        <div className="flex-col w-full h-full">{children}</div>
+      <div className="flex overflow-hidden">
+        <Sidebar className="hidden md:flex shadow-md" menus={menus} />
+        <ScrollArea className="flex-col w-full h-full overflow-auto bg-transparent p-2">
+          {children}
+        </ScrollArea>
       </div>
     </div>
   );
