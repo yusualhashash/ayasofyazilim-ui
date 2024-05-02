d7fc4d378742dbfeb0c1dd1e5a0b780942bbc8b3
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-05-02 09:40:19 +0300

    refactor: seperate card list data

--- src/organisms/card-list/card-list.stories.tsx
+++ src/organisms/card-list/card-list.stories.tsx
@@ -1,7 +1,7 @@
 import { Meta, StoryFn } from '@storybook/react';
-import React from 'react';
 
 import CardList from '.';
+import { cards } from './data';
 
 export default {
   component: CardList,
@@ -16,24 +16,5 @@ const Template: StoryFn<typeof CardList> = (args) => <CardList {...args} />;
 export const CardListStory = Template.bind({});
 
 CardListStory.args = {
-  cards: [
-    {
-      title: 'Paid',
-      content: '15%',
-      description: 'Number of paid taxes',
-      footer: 'Your target is 100%',
-    },
-    {
-      title: 'People',
-      content: '15k',
-      description: 'Number of people in the system',
-      footer: 'Your target is 20K',
-    },
-    {
-      title: 'WIP',
-      content: '1',
-      description: 'Number of WIP refunds',
-      footer: 'Your target is 0',
-    },
-  ],
+  cards,
 };
