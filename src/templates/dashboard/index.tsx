aa10893c1ce5cafaac59c342a33df607911ad86e
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-05-02 09:54:01 +0300

    feat: add no data case to dashboard

--- src/templates/dashboard/index.tsx
+++ src/templates/dashboard/index.tsx
@@ -3,14 +3,21 @@ import DataTable from '../../molecules/tables';
 import { DashboardProps } from './types';
 
 export default function Dashboard({ cards, data, columns }: DashboardProps) {
+  let showColumns = true;
+  if (!data || !columns) showColumns = false;
+
   return (
     <div className="flex flex-col items-center justify-start h-full w-full">
-      <div className="flex-row p-4">
-        <CardList cards={cards} />
-      </div>
-      <div className="w-10/12 flex-row p-4 m-4">
-        <DataTable columns={columns} data={data} />
-      </div>
+      {cards && (
+        <div className="flex-row p-4">
+          <CardList cards={cards} />
+        </div>
+      )}
+      {showColumns && (
+        <div className="w-10/12 flex-row p-4 m-4">
+          <DataTable columns={columns} data={data} />
+        </div>
+      )}
     </div>
   );
 }
