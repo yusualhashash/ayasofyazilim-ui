d03d8a1a3f64b47f3cca0f8336a18fdc9e391741
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-05-09 10:28:48 +0300

    feat: use autoform in tables

--- src/molecules/tables/index.tsx
+++ src/molecules/tables/index.tsx
@@ -40,9 +40,10 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog';
-import { Label } from '@/components/ui/label';
+import AutoForm from '../../organisms/auto-form';
 
 export type tableAction = {
+  autoFormArgs: any;
   callback: () => void;
   cta: string;
   description: string;
@@ -121,36 +122,30 @@ export default function DataTable<TData, TValue>({
               ))}
           </DropdownMenuContent>
         </DropdownMenu>
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button variant="outline">{action?.cta}</Button>
-          </DialogTrigger>
-          <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>Edit profile</DialogTitle>
-              <DialogDescription>{action?.description}</DialogDescription>
-            </DialogHeader>
-            <div className="grid gap-4 py-4">
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="name" className="text-right">
-                  Name
-                </Label>
-                <Input id="name" value="Pedro Duarte" className="col-span-3" />
+        {action ? (
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="outline">{action?.cta}</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>{action?.cta}</DialogTitle>
+                <DialogDescription>{action?.description}</DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <AutoForm {...action?.autoFormArgs}>
+                  {action?.autoFormArgs?.children}
+                  {/* <AutoFormSubmit>Send now</AutoFormSubmit> */}
+                </AutoForm>
               </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="username" className="text-right">
-                  Username
-                </Label>
-                <Input id="username" value="@peduarte" className="col-span-3" />
-              </div>
-            </div>
-            <DialogFooter>
-              <Button type="submit" onClick={action?.callback}>
-                Save changes
-              </Button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
+              <DialogFooter>
+                <Button type="submit" onClick={action?.callback}>
+                  Save changes
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        ) : null}
       </div>
       <div className="rounded-md border">
         <Table>
--- src/molecules/tables/tables.stories.tsx
+++ src/molecules/tables/tables.stories.tsx
@@ -1,13 +1,47 @@
 import { Meta, StoryObj } from '@storybook/react';
 
+import { z } from 'zod';
 import Table, { tableAction } from '.';
 import { data } from './data';
 import { columns } from './columns';
+
+const formSchema = z.object({
+  username: z
+    .string({
+      required_error: 'Username is required.',
+    })
+    .min(2, {
+      message: 'Username must be at least 2 characters.',
+    }),
+
+  password: z
+    .string({
+      required_error: 'Password is required.',
+    })
+    .describe('Your secure password')
+    .min(8, {
+      message: 'Password must be at least 8 characters.',
+    }),
+});
+
+const autoFormArgs = {
+  formSchema,
+  fieldConfig: {
+    password: {
+      inputProps: {
+        type: 'password',
+        placeholder: '••••••••',
+      },
+    },
+  },
+  children: <div>Extra data</div>,
+};
 
 const action: tableAction = {
   cta: 'New Record',
   description: 'Ad New Record',
   callback: () => alert('Added'),
+  autoFormArgs,
 };
 
 export default { component: Table } as Meta<typeof Table>;
