3f544c835a3d11d010b60f8e32dac99a2bf64e21
Eren <eren.odaci@ayasofyazilim.com>
2024-05-21 11:43:17 +0300

    chore: replace Section Layout import

--- src/templates/section-layout/section-layout.stories.tsx
+++ src/templates/section-layout/section-layout.stories.tsx
@@ -1,7 +1,7 @@
 import { Meta, StoryFn } from '@storybook/react';
 import React from 'react';
 
-import SectionLayout, { defaultProps } from '.';
+import { SectionLayout, defaultProps } from '.';
 
 export default {
   component: SectionLayout,
