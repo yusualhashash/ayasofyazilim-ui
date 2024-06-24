931d54bfc9fcb0436c08ed4dacb28bd5bcb142c4
Eren <ahmeterenodaci@gmail.com>
2024-06-24 14:58:24 +0300

    feat: scroll to top

--- src/templates/main-layout/index.tsx
+++ src/templates/main-layout/index.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { ChevronUp } from 'lucide-react';
+import { useEffect, useRef, useState } from 'react';
 import { cn } from '@/lib/utils';
 
 export type mainLayoutProps = {
@@ -13,6 +17,27 @@ export default function MainLayout({
   children,
   mainClassName,
 }: mainLayoutProps) {
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const handleScroll = () => {
+    if (scrollRef.current) {
+      const currentPosition = scrollRef.current.scrollTop;
+      setIsScrolled(currentPosition > 0);
+    }
+  };
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.addEventListener('scroll', handleScroll);
+    }
+
+    return () => {
+      if (scrollRef.current) {
+        scrollRef.current.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, []);
+
   return (
     <div className="h-dvh grid grid-rows-[max-content_1fr] overflow-hidden">
       {HeaderComponent}
@@ -21,12 +46,25 @@ export default function MainLayout({
         {SidebarComponent}
 
         <main
+          ref={scrollRef}
           className={cn(
             'flex min-h-[calc(100vh_-_theme(spacing.16))] gap-4 p-4 md:gap-8 md:p-10 w-full',
             mainClassName
           )}
         >
           {children}
+          {isScrolled && (
+            <button
+              type="button"
+              aria-label="Scroll to Top"
+              className="fixed bottom-5 z-50 right-5 p-2 bg-gray-600 hover:bg-gray-700 rounded-full cursor-pointer"
+              onClick={() =>
+                scrollRef.current?.scrollTo({ top: 0, behavior: 'smooth' })
+              }
+            >
+              <ChevronUp className="w-6 h-6" color="white" />
+            </button>
+          )}
         </main>
       </div>
     </div>
