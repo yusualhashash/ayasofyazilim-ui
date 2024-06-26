8b9ecbedbaa9d0a8097dcc749c7bf3729d9915e5
Ertuğrulcan Bakas <ecbakas@gmail.com>
2024-06-26 09:39:59 +0300

    chore:chart colors

--- src/molecules/area-chart/index.tsx
+++ src/molecules/area-chart/index.tsx
@@ -51,9 +51,7 @@ const LegendItem = ({
       className={cn(
         // base
         'group inline-flex flex-nowrap items-center gap-1.5 whitespace-nowrap rounded px-2 py-1 transition',
-        hasOnValueChange
-          ? 'cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-800'
-          : 'cursor-default'
+        hasOnValueChange ? 'cursor-pointer hover:bg-gray-100' : 'cursor-default'
       )}
       onClick={(e) => {
         e.stopPropagation();
@@ -73,9 +71,8 @@ const LegendItem = ({
           // base
           'truncate whitespace-nowrap text-xs',
           // text color
-          'text-gray-700 dark:text-gray-300',
-          hasOnValueChange &&
-            'group-hover:text-gray-900 dark:group-hover:text-gray-50',
+          'text-gray-700',
+          hasOnValueChange && 'group-hover:text-gray-900',
           activeLegend && activeLegend !== name ? 'opacity-40' : 'opacity-100'
         )}
       >
@@ -121,8 +118,8 @@ const ScrollButton = ({ icon, onClick, disabled }: ScrollButtonProps) => {
         // base
         'group inline-flex size-5 items-center truncate rounded transition',
         disabled
-          ? 'cursor-not-allowed text-gray-400 dark:text-gray-600'
-          : 'cursor-pointer text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-300 dark:hover:bg-gray-800 dark:hover:text-gray-50'
+          ? 'cursor-not-allowed text-gray-400'
+          : 'cursor-pointer text-gray-700 hover:bg-gray-100 hover:text-gray-900'
       )}
       disabled={disabled}
       onClick={(e) => {
@@ -286,7 +283,7 @@ const Legend = React.forwardRef<HTMLOListElement, LegendProps>((props, ref) => {
               // base
               'absolute bottom-0 right-0 top-0 flex h-full items-center justify-center pr-1',
               // background color
-              'bg-white dark:bg-gray-950'
+              'bg-white '
             )}
           >
             <ScrollButton
@@ -381,7 +378,7 @@ const ChartTooltipRow = ({ value, name, color }: ChartTooltipRowProps) => (
           // commmon
           'whitespace-nowrap text-right',
           // text color
-          'text-gray-700 dark:text-gray-300'
+          'text-gray-700'
         )}
       >
         {name}
@@ -392,7 +389,7 @@ const ChartTooltipRow = ({ value, name, color }: ChartTooltipRowProps) => (
         // base
         'whitespace-nowrap text-right font-medium tabular-nums',
         // text color
-        'text-gray-900 dark:text-gray-50'
+        'text-gray-900'
       )}
     >
       {value}
@@ -429,9 +426,9 @@ const ChartTooltip = ({
           // base
           'rounded-md border text-sm shadow-md',
           // border color
-          'border-gray-200 dark:border-gray-800',
+          'border-gray-200',
           // background color
-          'bg-white dark:bg-gray-950'
+          'bg-white'
         )}
       >
         <div
@@ -445,7 +442,7 @@ const ChartTooltip = ({
               // base
               'font-medium',
               // text color
-              'text-gray-900 dark:text-gray-50'
+              'text-gray-900 '
             )}
           >
             {label}
@@ -679,7 +676,7 @@ const AreaChart = React.forwardRef<HTMLDivElement, AreaChartProps>(
           >
             {showGridLines ? (
               <CartesianGrid
-                className={cn('stroke-gray-200 stroke-1 dark:stroke-gray-800')}
+                className={cn('stroke-gray-200 stroke-1 ')}
                 horizontal={true}
                 vertical={false}
               />
@@ -701,7 +698,7 @@ const AreaChart = React.forwardRef<HTMLDivElement, AreaChartProps>(
                 // base
                 'text-xs',
                 // text fill
-                'fill-gray-500 dark:fill-gray-500'
+                'fill-gray-500'
               )}
               tickLine={false}
               axisLine={false}
@@ -711,7 +708,7 @@ const AreaChart = React.forwardRef<HTMLDivElement, AreaChartProps>(
                 <Label
                   position="insideBottom"
                   offset={-20}
-                  className="fill-gray-800 text-sm font-medium dark:fill-gray-200"
+                  className="fill-gray-800 text-sm font-medium"
                 >
                   {xAxisLabel}
                 </Label>
@@ -731,7 +728,7 @@ const AreaChart = React.forwardRef<HTMLDivElement, AreaChartProps>(
                 // base
                 'text-xs',
                 // text fill
-                'fill-gray-500 dark:fill-gray-500'
+                'fill-gray-500'
               )}
               tickFormatter={
                 type === 'percent' ? valueToPercent : valueFormatter
@@ -744,7 +741,7 @@ const AreaChart = React.forwardRef<HTMLDivElement, AreaChartProps>(
                   style={{ textAnchor: 'middle' }}
                   angle={-90}
                   offset={-15}
-                  className="fill-gray-800 text-sm font-medium dark:fill-gray-200"
+                  className="fill-gray-800 text-sm font-medium"
                 >
                   {yAxisLabel}
                 </Label>
@@ -865,7 +862,7 @@ const AreaChart = React.forwardRef<HTMLDivElement, AreaChartProps>(
                       return (
                         <Dot
                           className={cn(
-                            'stroke-white dark:stroke-gray-950',
+                            'stroke-white',
                             onValueChange ? 'cursor-pointer' : '',
                             getColorClassName(
                               categoryColors.get(
@@ -921,7 +918,7 @@ const AreaChart = React.forwardRef<HTMLDivElement, AreaChartProps>(
                             strokeLinejoin={strokeLinejoin}
                             strokeWidth={strokeWidth}
                             className={cn(
-                              'stroke-white dark:stroke-gray-950',
+                              'stroke-white',
                               onValueChange ? 'cursor-pointer' : '',
                               getColorClassName(
                                 categoryColors.get(
