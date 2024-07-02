69cba96eb8951cc0e22f40908f1ae66f48874d75
Eren <ahmeterenodaci@gmail.com>
2024-07-02 14:56:46 +0300

    feat: add custom icon & color support to accordion stepper header

--- src/organisms/accordion-stepper-header/index.tsx
+++ src/organisms/accordion-stepper-header/index.tsx
@@ -1,27 +1,44 @@
 'use client';
 
-import { Circle, CircleCheckBig } from 'lucide-react';
+import { Circle, CircleCheckBig, LucideIcon } from 'lucide-react';
 import React from 'react';
 import { AccordionTrigger } from '@/components/ui/accordion';
 
 interface IAccordionStepperHeaderProps {
   checked: boolean;
   children: React.ReactNode;
+  customCheckedIcon?: LucideIcon;
+  customCheckedIconColor?: string;
+  customUncheckedIcon?: LucideIcon;
+  customUncheckedIconColor?: string;
 }
 export const AccordionStepperHeader = ({
   children,
   checked,
-}: IAccordionStepperHeaderProps) => (
-  <div className="bg-muted/50 px-4">
-    <AccordionTrigger>
-      <div className="flex gap-4 items-center">
-        {checked ? (
-          <CircleCheckBig className="text-primary" />
-        ) : (
-          <Circle className="text-muted-foreground" />
-        )}
-        {children}
-      </div>
-    </AccordionTrigger>
-  </div>
-);
+  customCheckedIcon,
+  customUncheckedIcon,
+  customCheckedIconColor,
+  customUncheckedIconColor,
+}: IAccordionStepperHeaderProps) => {
+  const CheckedIcon = customCheckedIcon || CircleCheckBig;
+  const UncheckedIcon = customUncheckedIcon || Circle;
+
+  return (
+    <div className="bg-muted/50 px-4">
+      <AccordionTrigger>
+        <div className="flex gap-4 items-center">
+          {checked ? (
+            <CheckedIcon
+              className={customCheckedIconColor || 'text-emerald-600'}
+            />
+          ) : (
+            <UncheckedIcon
+              className={customUncheckedIconColor || 'text-muted-foreground'}
+            />
+          )}
+          {children}
+        </div>
+      </AccordionTrigger>
+    </div>
+  );
+};
