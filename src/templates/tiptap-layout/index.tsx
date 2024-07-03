c356ec295ee176238ba118b21bb27c53ceaa3ac6
Eren <ahmeterenodaci@gmail.com>
2024-07-03 14:13:51 +0300

    style: tiptap

--- src/templates/tiptap-layout/index.tsx
+++ src/templates/tiptap-layout/index.tsx
@@ -2,7 +2,9 @@
 
 import React, { useEffect, useRef, useState } from 'react';
 
+import { ChevronDown } from 'lucide-react';
 import { cn } from '@/lib/utils';
+import { useIsVisible } from '../../hooks/useIsVisible';
 
 export interface ISection {
   id: string;
@@ -15,16 +17,23 @@ export interface ISectionNavbarBase {
   sections: Array<ISection>;
 }
 export interface ISectionContentBase {
+  activeSectionId: string;
   className?: string;
   sectionContent?: JSX.Element;
+  sectionEnded: string;
   sectionId: string;
   setActiveSectionId: React.Dispatch<React.SetStateAction<string>>;
+  setSectionEnded: React.Dispatch<React.SetStateAction<string>>;
 }
+
 export interface ISectionContent {
+  activeSectionId: string;
   children?: JSX.Element;
   className?: string;
+  sectionEnded: string;
   sectionId: string;
   setActiveSectionId: React.Dispatch<React.SetStateAction<string>>;
+  setSectionEnded: React.Dispatch<React.SetStateAction<string>>;
 }
 export interface ISectionLayoutProps {
   className?: string;
@@ -36,53 +45,75 @@ export interface ISectionLayoutProps {
 const SectionNavbarBase = ({
   sections,
   activeSectionId,
-}: ISectionNavbarBase) => (
-  <div className="bg-white pt-3 pb-3 border-b sticky top-20 z-10">
-    <nav className="text-sm flex flex-row gap-5 justify-center">
-      {sections.map((section) => {
-        const className = `
-            hover:no-underline rounded-none bg-white ${section.id === activeSectionId ? `font-semibold text-primary sticky left-0 right-0` : 'text-muted-foreground hover:text-black'}`;
-        return (
-          <a href={`#${section.id}`} className={className} key={section.id}>
-            {section.name}
-          </a>
-        );
-      })}
-    </nav>
-  </div>
-);
+}: ISectionNavbarBase) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  return (
+    <div className="bg-white pt-3 pb-3 border-b sticky top-20 z-10">
+      <nav className="flex flex-row justify-center items-center">
+        <div className="text-sm flex flex-col md:flex-row md:flex-wrap gap-5 items-center md:justify-center">
+          {sections.map((section) => {
+            const className = `
+            hover:no-underline rounded-none items-center bg-white ${section.id === activeSectionId ? `font-semibold text-primary sticky left-0 right-0 flex flex-row` : showDropdown ? 'text-muted-foreground hover:text-black' : 'text-muted-foreground hover:text-black hidden md:block'} `;
+            return (
+              <a
+                href={`#${section.id}`}
+                className={className}
+                key={section.id}
+                onClick={() => setShowDropdown(false)}
+              >
+                {section.name}
+                {showDropdown && section.id === activeSectionId && (
+                  <ChevronDown
+                    className="items-center md:hidden text-muted-foreground ml-2 cursor-pointer transition-all"
+                    size={16}
+                    onClick={() => setShowDropdown((pre) => !pre)}
+                  />
+                )}
+              </a>
+            );
+          })}
+        </div>
+        {!showDropdown && (
+          <ChevronDown
+            className="items-center md:hidden text-muted-foreground ml-2 cursor-pointer transition-all"
+            size={16}
+            onClick={() => setShowDropdown((pre) => !pre)}
+          />
+        )}
+      </nav>
+    </div>
+  );
+};
 const SectionContentBase = ({
   setActiveSectionId,
   sectionId,
   sectionContent,
   className,
+  sectionEnded,
+  setSectionEnded,
+  activeSectionId,
 }: ISectionContentBase) => {
   const divRef = useRef(null);
-
+  const isVisible = useIsVisible(divRef, 0.05);
   useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          setActiveSectionId(sectionId);
-        }
-      },
-      { threshold: 0.5 }
-    );
-    if (divRef.current) {
-      observer.observe(divRef.current);
-    }
-    return () => {
-      if (divRef.current) {
-        observer.unobserve(divRef.current);
+    if (!isVisible) {
+      if (activeSectionId === sectionId) {
+        setSectionEnded(() => '');
       }
-    };
-  }, []);
-
+    }
+  }, [isVisible]);
+  useEffect(() => {
+    if (isVisible && sectionEnded === '') {
+      setActiveSectionId(() => sectionId);
+      setSectionEnded(() => sectionId);
+    }
+  }, [sectionEnded, isVisible]);
   return (
     <div
       id={`${sectionId}`}
       ref={divRef}
-      className={cn('w-full scroll-mt-20 pt-10', className)}
+      className={cn('w-full scroll-mt-20 pt-10 min-h-full', className)}
     >
       {sectionContent}
     </div>
@@ -93,14 +124,20 @@ export const SectionContent = ({
   children,
   className,
   sectionId,
+  activeSectionId,
   setActiveSectionId,
+  sectionEnded,
+  setSectionEnded,
 }: ISectionContent) => (
   <SectionContentBase
     key={sectionId}
     setActiveSectionId={setActiveSectionId}
+    activeSectionId={activeSectionId}
     sectionId={sectionId}
     sectionContent={children}
     className={className}
+    sectionEnded={sectionEnded}
+    setSectionEnded={setSectionEnded}
   />
 );
 
@@ -113,6 +150,7 @@ export function TiptapLayout({
   const [activeSectionId, setActiveSectionId] = useState(
     defaultActiveSectionId
   );
+  const [sectionEnded, setSectionEnded] = useState('');
 
   return (
     <div className={className}>
@@ -121,16 +159,21 @@ export function TiptapLayout({
         activeSectionId={activeSectionId}
       />
 
-      {sections.map((section) => (
-        <SectionContent
-          key={section.id}
-          sectionId={section.id}
-          setActiveSectionId={setActiveSectionId}
-          className={contentClassName}
-        >
-          {section?.value}
-        </SectionContent>
-      ))}
+      <div className="container">
+        {sections.map((section) => (
+          <SectionContent
+            key={section.id}
+            sectionId={section.id}
+            setActiveSectionId={setActiveSectionId}
+            className={contentClassName}
+            sectionEnded={sectionEnded}
+            activeSectionId={activeSectionId}
+            setSectionEnded={setSectionEnded}
+          >
+            {section?.value}
+          </SectionContent>
+        ))}
+      </div>
     </div>
   );
 }
