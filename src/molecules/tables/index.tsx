51738816b63dbfcbce2c6d75c1c8cdb8cf8714d0
yusualhashash <yusufalhashash2018@gmail.com>
2024-08-08 09:30:34 +0300

    refactor: edit table to work with editable features

--- src/molecules/tables/index.tsx
+++ src/molecules/tables/index.tsx
@@ -11,11 +11,12 @@ import {
   getFilteredRowModel,
   getSortedRowModel,
   useReactTable,
+  RowData,
 } from '@tanstack/react-table';
-import * as React from 'react';
-
 import Link from 'next/link';
-import { ReactNode, useEffect, useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { Trash2Icon } from 'lucide-react';
+import { ReactNode } from 'node_modules/react-resizable-panels/dist/declarations/src/vendor/react';
 import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
@@ -37,6 +38,16 @@ import {
 import AutoformDialog from '../dialog';
 import { columnsGenerator } from './columnsGenerator';
 import { normalizeName } from './utils';
+
+declare module '@tanstack/react-table' {
+  interface TableMeta<TData extends RowData> {
+    updateData: (
+      rowIndex: number,
+      columnId: string,
+      value: unknown | TData
+    ) => void;
+  }
+}
 
 export type tableAction = tableActionCommon &
   (tableActionNewPage | tableActionDialog | tableActionAction);
@@ -86,10 +97,12 @@ export type DataTableProps<TData> = {
   action?: tableAction | tableAction[];
   columnsData: columnsType;
   data: TData[];
+  editable?: boolean;
   fetchRequest?: any;
-  filterBy: string;
+  filterBy?: string;
   isLoading?: boolean;
   rowCount?: number;
+  showView?: boolean;
 };
 const SkeletonCell = () => <Skeleton className="w-20 h-3" />;
 
@@ -124,54 +137,19 @@ const ActionComponent = ({
     </Button>
   );
 };
-/**
- * Renders a data table with customizable columns, sorting, filtering, and selection capabilities.
- * The table can display data in either a custom format defined by the user or automatically generate columns based on provided data.
- * It supports loading states, custom actions, and can be filtered by a specific field.
- *
- * @param {DataTableProps<TData>} props - The properties for configuring the DataTable component.
- * @param {columnsType} props.columnsData - Configuration for the table columns. Can be either custom-defined columns or automatically generated based on data.
- * @param {TData[]} props.data - The data to be displayed in the table.
- * @param {string} props.filterBy - The field name to filter the table data by.
- * @param {tableAction} [props.action] - Optional. Configuration for an action that can be performed on the table data, such as adding a new row.
- * @param {boolean} [props.isLoading] - Optional. Indicates whether the table is in a loading state. Defaults to false.
- *
- * @returns {ReactElement} The rendered data table component.
- *
- * @template TData - The type of the data objects that will be displayed in the table.
- * @template TValue - The type of the values within each data object. This is used when defining custom columns.
- *
- * @example
- * // Example usage of DataTable with auto-generated columns
- * const usersData = [{ id: 1, name: 'John Doe', role: 'Admin' }];
- * const columnsConfig = {
- *   type: 'Auto',
- *   data: {
- *     autoFormArgs: {},
- *     callback: () => {},
- *     excludeList: ['id'],
- *     onDelete: () => {},
- *     onEdit: () => {},
- *     tableType: 'UserTable',
- *   },
- * };
- *
- * <DataTable
- *   columnsData={columnsConfig}
- *   data={usersData}
- *   filterBy="name"
- * />
- */
+
 export default function DataTable<TData, TValue>({
   columnsData,
   data,
   filterBy,
   action,
-  isLoading,
+  isLoading = false,
   rowCount,
   fetchRequest,
+  showView = true,
+  editable = false,
 }: DataTableProps<TData>) {
-  let tableData = data;
+  const [tableData, setTableData] = useState<TData[]>(data || []);
   const isMultipleActionProvided = Array.isArray(action);
   const [isOpen, setIsOpen] = useState(false);
   let defaultAction: tableAction | undefined;
@@ -186,19 +164,28 @@ export default function DataTable<TData, TValue>({
   const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
   const [rowSelection, setRowSelection] = useState({});
 
+  useEffect(() => {
+    if (isLoading) {
+      setTableData(Array(6).fill({}) as TData[]);
+    } else {
+      setTableData(data);
+    }
+  }, [isLoading, data]);
+
   let columns: ColumnDef<any, any>[] = [];
   if (columnsData.type === 'Auto') {
     columns = columnsGenerator(columnsData.data as autoColumnGnerator);
   } else {
     columns = columnsData.data as ColumnDef<TData, TValue>[];
   }
+
   if (isLoading) {
-    tableData = Array(6).fill({});
     columns = columns.map((column) => ({
       ...column,
       cell: SkeletonCell,
     }));
   }
+
   const table = useReactTable({
     data: tableData,
     columns,
@@ -223,7 +210,24 @@ export default function DataTable<TData, TValue>({
       columnVisibility,
       rowSelection,
     },
+    meta: {
+      updateData: (rowIndex, columnId, value) => {
+        setTableData((old) =>
+          old.map((row, index) => {
+            if (index === rowIndex) {
+              return {
+                ...row,
+                [columnId]: value,
+              };
+            }
+            return row;
+          })
+        );
+      },
+    },
   });
+
+  const selectedRows = table?.getSelectedRowModel()?.rows || [];
 
   useEffect(() => {
     fetchRequest?.(table.getState().pagination.pageIndex);
@@ -233,6 +237,21 @@ export default function DataTable<TData, TValue>({
     if (isLoading) return 'Loading...';
     return `${table.getFilteredSelectedRowModel().rows.length} of ${table.getFilteredRowModel().rows.length} row(s) selected.`;
   }
+
+  const handleAddRow = () => {
+    const newRow = {} as TData;
+    setTableData((prevData) => [...prevData, newRow]);
+  };
+
+  const handleRemoveSelected = useCallback(() => {
+    const selectedRows = table
+      .getSelectedRowModel()
+      .rows.map((row: any) => row.index);
+    setTableData((old) =>
+      old.filter((_row, index) => !selectedRows.includes(index))
+    );
+    table.resetRowSelection();
+  }, [tableData]);
 
   return (
     <div className="w-full">
@@ -245,37 +264,49 @@ export default function DataTable<TData, TValue>({
         />
       )}
       <div className="flex items-center py-4 gap-2">
-        <Input
-          disabled={isLoading}
-          placeholder={`Filter ${filterBy}s...`}
-          value={(table.getColumn(filterBy)?.getFilterValue() as string) ?? ''}
-          onChange={(event) =>
-            table.getColumn(filterBy)?.setFilterValue(event.target.value)
-          }
-          className="max-w-sm"
-        />
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button disabled={isLoading} variant="outline" className="ml-auto">
-              View <ChevronDownIcon className="ml-2 h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            {table
-              .getAllColumns()
-              .filter((column) => column.getCanHide())
-              .map((column) => (
-                <DropdownMenuCheckboxItem
-                  key={column.id}
-                  className="capitalize"
-                  checked={column.getIsVisible()}
-                  onCheckedChange={(value) => column.toggleVisibility(!!value)}
-                >
-                  {normalizeName(column.id)}
-                </DropdownMenuCheckboxItem>
-              ))}
-          </DropdownMenuContent>
-        </DropdownMenu>
+        {filterBy && (
+          <Input
+            disabled={isLoading}
+            placeholder={`Filter ${filterBy}s...`}
+            value={
+              (table.getColumn(filterBy)?.getFilterValue() as string) ?? ''
+            }
+            onChange={(event) =>
+              table.getColumn(filterBy)?.setFilterValue(event.target.value)
+            }
+            className="max-w-sm"
+          />
+        )}
+        {showView === true && (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                disabled={isLoading}
+                variant="outline"
+                className="ml-auto"
+              >
+                View <ChevronDownIcon className="ml-2 h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              {table
+                .getAllColumns()
+                .filter((column) => column.getCanHide())
+                .map((column) => (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {normalizeName(column.id)}
+                  </DropdownMenuCheckboxItem>
+                ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
 
         <div className="flex">
           <ActionComponent
@@ -350,7 +381,7 @@ export default function DataTable<TData, TValue>({
               table.getRowModel().rows.map((row) => (
                 <TableRow
                   key={row.id}
-                  data-state={row.getIsSelected() && 'selected'}
+                  data-state={row.getIsSelected() ? 'selected' : undefined}
                   className="whitespace-nowrap"
                 >
                   {row.getVisibleCells().map((cell) => (
@@ -383,24 +414,48 @@ export default function DataTable<TData, TValue>({
         <div className="flex-1 text-sm text-muted-foreground">
           {selectedRowsText()}
         </div>
-        <div className="space-x-2">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Previous
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Next
-          </Button>
-        </div>
+        {editable ? (
+          <div className="footer-buttons bg-white">
+            <div className="flex w-full justify-end items-end gap-5 py-3 px-3">
+              {selectedRows?.length > 0 && (
+                <Button
+                  className="remove-button w-44 h-10 flex items-center justify-center"
+                  variant="outline"
+                  onClick={handleRemoveSelected}
+                >
+                  Remove Selected
+                  <Trash2Icon className="ml-2 h-4 w-4" />
+                </Button>
+              )}
+              <Button
+                className="add-button w-44 h-10 flex items-center justify-center"
+                variant="outline"
+                onClick={handleAddRow}
+              >
+                Add New +
+              </Button>
+            </div>
+          </div>
+        ) : (
+          <div className="space-x-2">
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              Previous
+            </Button>
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              Next
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   );
