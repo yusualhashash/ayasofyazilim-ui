272d04647b54058f2fb3657156bf2a0561f8ee0e
Ertuğrulcan Bakas <ecbakas@gmail.com>
2024-08-21 11:53:36 +0300

    feat:Date Picker disabled & className props

--- @/components/ui/date-picker.tsx
+++ @/components/ui/date-picker.tsx
@@ -17,16 +17,20 @@ export const DatePicker = forwardRef<
   {
     date?: Date;
     setDate: (date?: Date) => void;
+    disabled?: boolean;
+    className?: string;
   }
->(function DatePickerCmp({ date, setDate }, ref) {
+>(function DatePickerCmp({ date, setDate, disabled, className }, ref) {
   return (
     <Popover>
       <PopoverTrigger asChild>
         <Button
+          disabled={disabled}
           variant={'outline'}
           className={cn(
             'w-full justify-start text-left font-normal',
-            !date && 'text-muted-foreground'
+            !date && 'text-muted-foreground',
+            className
           )}
         >
           <CalendarIcon className="mr-2 h-4 w-4" />
