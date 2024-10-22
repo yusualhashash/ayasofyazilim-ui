e9d4be6ef7b0a6bd97a93fa0ef34268981cf8486
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-10-22 16:47:55 +0300

    chore: filter by id

--- src/molecules/tables/tables.stories.tsx
+++ src/molecules/tables/tables.stories.tsx
@@ -291,7 +291,7 @@ export const DetailedFilter: StoryObj<typeof Table> = {
             return parsedFilter[filterKey].includes(tableItem.status);
           }
           if (filterKey === 'select-async') {
-            return parsedFilter[filterKey].includes(tableItem.email);
+            return parsedFilter[filterKey].includes(tableItem.id);
           }
           return true;
         });
@@ -343,7 +343,7 @@ export const DetailedFilter: StoryObj<typeof Table> = {
         type: 'select-async',
         displayName: 'select-async',
         value: '',
-        filterProperty: 'email',
+        filterProperty: 'id',
         showProperty: 'email',
       },
       {
