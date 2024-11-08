1588b944524d26ac38bbef78d0e5dc355f11f6d1
Eren <ahmeterenodaci@gmail.com>
2024-11-08 14:01:40 +0300

    chore(tanstack-table): add storybook examples

--- src/molecules/tanstack-table/tanstack-table.stories.tsx
+++ src/molecules/tanstack-table/tanstack-table.stories.tsx
@@ -1,9 +1,6 @@
-import { PersonIcon } from '@radix-ui/react-icons';
 import { Meta, StoryFn } from '@storybook/react';
-import { Building2 } from 'lucide-react';
 import TanstackTable from '.';
-import { users } from './tanstack-table.stories.data';
-import { tanstackTableCreateColumnsByRowData } from './utils';
+import { actions, col, users } from './tanstack-table.stories.data';
 
 export default {
   component: TanstackTable,
@@ -14,31 +11,11 @@ export default {
 } as Meta<typeof TanstackTable>;
 
 const template: StoryFn<typeof TanstackTable> = (args) => (
-  <TanstackTable {...args} />
+  <TanstackTable {...args} actions={actions} data={users} columns={col} />
 );
+
 export const Default = template.bind({});
 Default.args = {
-  columns: tanstackTableCreateColumnsByRowData({
-    row: users[0],
-    languageData: { userName: 'Kullanıcı Adı' },
-    links: {
-      userName: {
-        targetAccessorKey: 'id',
-        prefix: 'http://192.168.1.105:1453/tr/app/admin',
-        suffix: '/edit',
-      },
-      email: {
-        prefix: 'http://192.168.1.105:1453/tr/app/',
-      },
-    },
-    faceted: {
-      status: [
-        { value: 'inactive', label: 'Inactive', icon: Building2 },
-        { value: 'active', label: 'Active', icon: PersonIcon },
-      ],
-    },
-  }),
-  data: users,
   filters: {
     textFilters: ['userName'],
     facetedFilters: {
