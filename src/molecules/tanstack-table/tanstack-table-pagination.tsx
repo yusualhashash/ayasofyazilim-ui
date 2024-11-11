9833d582f60567f061cc2628520a068dd3eb5532
Eren <ahmeterenodaci@gmail.com>
2024-11-11 16:37:38 +0300

    style(tanstack-table): add padding to pagination

--- src/molecules/tanstack-table/tanstack-table-pagination.tsx
+++ src/molecules/tanstack-table/tanstack-table-pagination.tsx
@@ -24,7 +24,7 @@ export function TanstackTablePagination<TData>(
 ) {
   const { table } = props;
   return (
-    <div className="flex items-center justify-between px-2">
+    <div className="flex items-center justify-between px-2 pb-2">
       <div className="flex-1 text-sm text-muted-foreground">
         {table.getFilteredSelectedRowModel().rows.length} of{' '}
         {table.getFilteredRowModel().rows.length} row(s) selected.
