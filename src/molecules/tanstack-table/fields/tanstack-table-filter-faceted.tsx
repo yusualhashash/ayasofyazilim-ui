b431da9e5f9c1de9e573c003e801cbc58fa980ac
Eren <ahmeterenodaci@gmail.com>
2024-11-13 13:55:17 +0300

    feat(tanstack-table): server side filtering

--- src/molecules/tanstack-table/fields/tanstack-table-filter-faceted.tsx
+++ src/molecules/tanstack-table/fields/tanstack-table-filter-faceted.tsx
@@ -1,6 +1,7 @@
 import { CheckIcon, PlusCircledIcon } from '@radix-ui/react-icons';
 import { Column } from '@tanstack/react-table';
 
+import { useEffect } from 'react';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import {
@@ -24,6 +25,7 @@ import { TanstackTableFacetedFilterType } from '../types';
 interface DataTableFacetedFilterProps<TData, TValue> {
   accessorKey: string;
   column?: Column<TData, TValue>;
+  onFilter: (accessorKey: string, selectedValues: string) => void;
   options: TanstackTableFacetedFilterType[];
 }
 
@@ -31,10 +33,16 @@ export function TanstackTableFacetedFilter<TData, TValue>({
   column,
   accessorKey,
   options,
+  onFilter,
 }: DataTableFacetedFilterProps<TData, TValue>) {
   const title = column?.columnDef?.meta?.toString() || accessorKey;
   const facets = column?.getFacetedUniqueValues();
   const selectedValues = new Set(column?.getFilterValue() as string[]);
+
+  useEffect(() => {
+    onFilter(accessorKey, Array.from(selectedValues).join(','));
+  }, [selectedValues]);
+
   return (
     <Popover>
       <PopoverTrigger asChild>
--- src/molecules/tanstack-table/fields/tanstack-table-toolbar.tsx
+++ src/molecules/tanstack-table/fields/tanstack-table-toolbar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Table } from '@tanstack/react-table';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { TanstackTableFacetedFilter } from './tanstack-table-filter-faceted';
 import { TanstackTableTextFilter } from './tanstack-table-filter-text';
 import { TanstackTableViewOptions } from './tanstack-table-view-options';
@@ -24,33 +25,53 @@ export const TanstackTableToolbar = <TData,>({
   selectedRowAction,
   tableActions,
   setTableAction,
-}: TanstackTableToolbarProps<TData>) => (
-  <div className="flex w-full items-center justify-between">
-    <div className="flex flex-1 items-center space-x-2">
-      {filters?.textFilters &&
-        filters.textFilters.map((accessorKey) => (
-          <TanstackTableTextFilter
-            key={accessorKey}
-            column={table.getColumn(accessorKey)}
-            accessorKey={accessorKey}
-          />
-        ))}
+}: TanstackTableToolbarProps<TData>) => {
+  const { replace } = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  function onFilter(accessorKey: string, selectedValues: string) {
+    const params = new URLSearchParams(searchParams.toString());
+    if (selectedValues) {
+      params.set(accessorKey, selectedValues);
+    } else {
+      params.delete(accessorKey);
+    }
+    if (params.get(accessorKey) !== searchParams.get(accessorKey)) {
+      replace(`${pathname}?${params.toString()}`);
+    }
+  }
 
-      {filters?.facetedFilters &&
-        Object.keys(filters.facetedFilters)?.map((column) => (
-          <TanstackTableFacetedFilter
-            key={column}
-            column={table.getColumn(column)}
-            accessorKey={column}
-            options={filters?.facetedFilters?.[column] ?? []}
-          />
-        ))}
+  return (
+    <div className="flex w-full items-center justify-between">
+      <div className="flex flex-1 items-center space-x-2">
+        {filters?.textFilters &&
+          filters.textFilters.map((accessorKey) => (
+            <TanstackTableTextFilter
+              key={accessorKey}
+              column={table.getColumn(accessorKey)}
+              accessorKey={accessorKey}
+            />
+          ))}
+
+        {filters?.facetedFilters &&
+          Object.keys(filters.facetedFilters)?.map((column) => (
+            <TanstackTableFacetedFilter
+              key={column}
+              column={table.getColumn(column)}
+              accessorKey={column}
+              onFilter={(accessorKey, selectedValues) =>
+                onFilter(accessorKey, selectedValues)
+              }
+              options={filters?.facetedFilters?.[column] ?? []}
+            />
+          ))}
+      </div>
+      <TanstackTableViewOptions
+        table={table}
+        selectedRowAction={selectedRowAction}
+        tableActions={tableActions}
+        setTableAction={setTableAction}
+      />
     </div>
-    <TanstackTableViewOptions
-      table={table}
-      selectedRowAction={selectedRowAction}
-      tableActions={tableActions}
-      setTableAction={setTableAction}
-    />
-  </div>
-);
+  );
+};
