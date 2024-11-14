3f7f41bb6b7d4ed6cfdb94fed22c8f9f4a77edce
yusualhashash <yusufalhashash2018@gmail.com>
2024-11-14 15:24:34 +0300

    feat: add password input to the autoform

---
+++ src/organisms/auto-form/fields/password.tsx
@@ -0,0 +1,48 @@
+import { FormControl, FormItem, FormMessage } from '@/components/ui/form';
+import AutoFormLabel from '../common/label';
+import AutoFormTooltip from '../common/tooltip';
+import { AutoFormInputComponentProps } from '../types';
+import { cn } from '@/lib/utils';
+import { Skeleton } from '@/components/ui/skeleton';
+import { PasswordInput } from '../../../molecules/password-input';
+
+export default function AutoFormPasword({
+  label,
+  isRequired,
+  fieldConfigItem,
+  fieldProps,
+}: AutoFormInputComponentProps) {
+  const { showLabel: _showLabel, ...fieldPropsWithoutShowLabel } = fieldProps;
+  const showLabel = _showLabel === undefined ? true : _showLabel;
+  const type = fieldProps.type || 'text';
+  const params = fieldPropsWithoutShowLabel;
+  delete params.containerClassName;
+  delete params.isLoading;
+  if (fieldProps.isLoading)
+    return (
+      <div
+        className={cn(
+          'flex w-full flex-col justify-start space-y-2',
+          fieldProps.containerClassName
+        )}
+      >
+        <Skeleton className="w-1/2 h-3" />
+        <Skeleton className="w-full h-9" />
+      </div>
+    );
+  return (
+    <FormItem
+      className={cn(
+        'flex w-full flex-col px-px justify-start',
+        fieldProps.containerClassName
+      )}
+    >
+      {showLabel && <AutoFormLabel label={label} isRequired={isRequired} />}
+      <FormControl>
+        <PasswordInput type={type} {...params} />
+      </FormControl>
+      <AutoFormTooltip fieldConfigItem={fieldConfigItem} />
+      <FormMessage />
+    </FormItem>
+  );
+}
