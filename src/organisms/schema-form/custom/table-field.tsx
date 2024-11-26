0319c6fe401bcf706d23169971f69c7763c9510c
Ertuğrulcan Bakas <ecbakas@gmail.com>
2024-11-26 12:45:22 +0300

    feat:(SchemaForm): TableField label

--- src/organisms/schema-form/custom/table-field.tsx
+++ src/organisms/schema-form/custom/table-field.tsx
@@ -3,6 +3,7 @@ import { useMemo } from 'react';
 import TanstackTable from '../../../molecules/tanstack-table';
 import { TanstackTableProps } from '../../../molecules/tanstack-table/types';
 import { ErrorSchemaTemplate } from '../fields';
+import { Label } from '@/components/ui/label';
 
 type TableFieldProps<TData> = Omit<
   TanstackTableProps<TData, TData>,
@@ -11,9 +12,12 @@ type TableFieldProps<TData> = Omit<
 
 export function TableField<TData>({ ...tableProps }: TableFieldProps<TData>) {
   const Field = (props: FieldProps) => {
+    const { uiSchema } = props;
+    const title = uiSchema?.['ui:title'];
     const memory = useMemo(
       () => (
-        <div className="flex flex-col">
+        <div className="flex flex-col border rounded-md p-4">
+          {title && <Label>{title}</Label>}
           <TanstackTable
             {...tableProps}
             onTableDataChange={(data) => {
