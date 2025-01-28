e16b778bbdd490fea21f0cf73a491b39cc368e04
Ertuğrulcan Bakas <ecbakas@gmail.com>
2025-01-28 13:25:49 +0300

    feat(date-picker): enhance DatePicker with time support and customizable icon display

--- src/molecules/date-picker/index.tsx
+++ src/molecules/date-picker/index.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { parseDate } from '@internationalized/date';
+import { parseDate, parseTime } from '@internationalized/date';
 import { CalendarIcon } from 'lucide-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   Button,
   DatePicker as DefaultDatePicker,
@@ -12,8 +12,9 @@ import {
   Popover,
 } from 'react-aria-components';
 import { cn } from '@/lib/utils';
-import { DateInput } from './datefield-rac';
+import { Separator } from '@/components/ui/separator';
 import { Calendar } from './calendar-rac';
+import { DateInput, TimeField } from './datefield-rac';
 
 export function DatePicker({
   label,
@@ -21,28 +22,54 @@ export function DatePicker({
   onChange,
   defaultValue,
   disabled = false,
+  useTime = false,
+  showIcon = true,
 }: {
   label?: string;
   disabled?: boolean;
   classNames?: {
     dateInput?: string;
   };
+  showIcon?: boolean;
+  useTime?: boolean;
   defaultValue?: Date;
   onChange?: (date: Date) => void;
 }) {
-  const [value, setValue] = useState(
+  const [dateValue, setDateValue] = useState(
     defaultValue && parseDate(defaultValue.toJSON().split('T').at(0) || '')
   );
+  const [timeValue, setTimeValue] = useState(
+    defaultValue &&
+      parseTime(defaultValue.toJSON().split('T').at(1)?.replace('Z', '') || '')
+  );
+  useEffect(() => {
+    if (!dateValue) return;
+    if (onChange) {
+      if (useTime && timeValue) {
+        onChange(
+          new Date(
+            dateValue.year,
+            dateValue.month - 1,
+            dateValue.day,
+            timeValue.hour,
+            timeValue.minute,
+            timeValue.second
+          )
+        );
+      } else {
+        onChange(new Date(dateValue.year, dateValue.month - 1, dateValue.day));
+      }
+    }
+  }, [dateValue, timeValue]);
   return (
     <DefaultDatePicker
       aria-label="x"
       className="space-y-2"
       isDisabled={disabled}
-      value={value}
+      value={dateValue}
       onChange={(date) => {
         if (date) {
-          setValue(date);
-          if (onChange) onChange(new Date(date.year, date.month - 1, date.day));
+          setDateValue(date);
         }
       }}
     >
@@ -50,12 +77,35 @@ export function DatePicker({
         <Label className="text-sm font-medium text-foreground">{label}</Label>
       )}
       <div className="flex">
-        <Group className="w-full">
-          <DateInput className={cn('pe-9', classNames?.dateInput)} />
+        <Group
+          className={cn(
+            'w-full flex border rounded-md h-9 pl-3 py-2 items-center gap-2 peer',
+            showIcon ? 'pr-9' : 'pr-3',
+            classNames?.dateInput
+          )}
+        >
+          <DateInput unstyled className="peer-focus:ring" />
+          {useTime && (
+            <>
+              <Separator orientation="vertical" />
+              <TimeField
+                value={timeValue}
+                onChange={(time) => {
+                  if (time) {
+                    setTimeValue(time);
+                  }
+                }}
+              >
+                <DateInput unstyled className="peer-focus:ring" />
+              </TimeField>
+            </>
+          )}
         </Group>
-        <Button className="z-10 -me-px -ms-9 flex w-9 items-center justify-center rounded-e-lg text-muted-foreground/80 outline-offset-2 transition-colors hover:text-foreground focus-visible:outline-none data-[focus-visible]:outline data-[focus-visible]:outline-2 data-[focus-visible]:outline-ring/70 border-none">
-          <CalendarIcon size={16} strokeWidth={2} />
-        </Button>
+        {showIcon && (
+          <Button className="z-10 -me-px -ms-9 flex w-9 items-center justify-center rounded-e-lg text-muted-foreground/80 outline-offset-2 transition-colors hover:text-foreground focus-visible:outline-none data-[focus-visible]:outline data-[focus-visible]:outline-2 data-[focus-visible]:outline-ring/70 border-none">
+            <CalendarIcon size={16} strokeWidth={2} />
+          </Button>
+        )}
       </div>
       <Popover
         placement="bottom end"
