f821ed1acd97cc6c0110d5e89117220e89c081b7
Ertuğrulcan Bakas <ecbakas@gmail.com>
2025-04-08 11:15:10 +0300

    fix(label): correct conditional rendering of Asterisk in FieldLabel component

--- src/organisms/schema-form/custom/label.tsx
+++ src/organisms/schema-form/custom/label.tsx
@@ -28,7 +28,7 @@ export function FieldLabel({
       className={cn('flex items-center text-slate-600', className)}
     >
       {label}
-      {!required ? <Asterisk className="size-3 text-destructive mb-1" /> : null}
+      {required ? <Asterisk className="size-3 text-destructive mb-1" /> : null}
       {description && description.length > 0 && (
         <TooltipProvider>
           <Tooltip>
