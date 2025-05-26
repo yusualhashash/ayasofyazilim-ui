942df9eb02afc7cb0c2218bc69eb5dcc7b00a900
Ertuğrulcan Bakas <ecbakas@gmail.com>
2025-05-26 14:29:15 +0300

    feat(SchemaForm): Add DateWidget to the list of widgets

--- src/organisms/schema-form/index.tsx
+++ src/organisms/schema-form/index.tsx
@@ -56,6 +56,7 @@ export function SchemaForm<T = unknown>({ ...props }: SchemaFormProps<T>) {
       TextWidget: CustomTextInput,
       TextareaWidget: CustomTextareaInput,
       DateTimeWidget: CustomDate,
+      DateWidget: CustomDate,
       password: PasswordInputWidget,
       email: EmailInputWidget,
       phone: CustomPhoneField,
