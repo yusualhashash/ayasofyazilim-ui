0dd5a7c552237c79354c4e919f314bb662094786
Ertuğrulcan Bakas <ecbakas@gmail.com>
2025-10-13 13:20:51 +0300

    chore:new alert variants

--- @/components/ui/alert.tsx
+++ @/components/ui/alert.tsx
@@ -10,8 +10,9 @@ const alertVariants = cva(
     variants: {
       variant: {
         default: 'bg-background text-foreground',
-        destructive:
-          'border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive',
+        destructive: 'border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive',
+        success: 'border-green-500/50 text-green-500 dark:border-green-500 [&>svg]:text-green-500',
+        information: 'border-blue-500/50 text-blue-500 dark:border-blue-500 [&>svg]:text-blue-500',
       },
     },
     defaultVariants: {
